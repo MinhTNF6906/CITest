53e0d37f52f18dc16309089234b18e014fc3273d
Minh Nguyen Duc Nhat <ndnminh6906@gmail.com>
2021-11-26 10:55:04 +0700

    add hi.cpp

---
+++ hi.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+#define FOR(i,l,r) for(int i = l; i <= r; ++i)
+#define FOD(i,r,l) for(int i = r; i >= l; --i)
+#define task "a"
+
+int a,b;
+int main (){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+//    #ifdef nhatminh
+//    freopen("a.inp", "r", stdin);
+//    freopen("a.out", "w", stdout);
+//    #endif // nhatminh
+    cin >> a >> b;
+    cout << a+b;
+    return 0;
+}
